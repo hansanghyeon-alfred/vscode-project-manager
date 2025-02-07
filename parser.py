aca6ac07c60cc681aa121e4d6ba48c9b957c0fa5
Hansanghyeon <hyeon@imweb.me>
2025-02-07 13:54:53 +0900

    windsurf 에디터 제공

--- parser.py
+++ parser.py
@@ -2,13 +2,19 @@ import json
 import sys
 import os
 
+# 현재행되고있는 파선파일의 경로를 가져와 
+projectDir = os.path.dirname(os.path.realpath(__file__))
+
 # Determine the file path based on the editor argument
+# 로그를 남겨줘
 if len(sys.argv) > 2 and sys.argv[2].lower() == 'windsurf':
     filePath = os.path.join(os.path.expanduser(
-        '~'), 'Library/Application Support/Windsurf/globalStorage/alefragnani.project-manager/projects.json')
+        '~'), 'Library/Application Support/Windsurf/User/globalStorage/alefragnani.project-manager/projects.json')
+    iconPath = os.path.join(projectDir, 'Windsurf.icns')
 else:
     filePath = os.path.join(os.path.expanduser(
         '~'), 'Library/Application Support/Code/User/globalStorage/alefragnani.project-manager/projects.json')
+    iconPath = os.path.join(projectDir, 'icon.png')
 
 # read file
 with open(filePath, 'r') as file:
@@ -24,7 +30,10 @@ for project in projects:
     item = {
         "title": project['name'],
         "subtitle": project['rootPath'].split('/')[-1],
-        "arg": project['rootPath']
+        "arg": project['rootPath'],
+        "icon": {
+            "path": iconPath
+        }
     }
     if len(sys.argv) > 1:
         if sys.argv[1].lower() in project['name'].lower():
